b595d21e6ca289b2faa3370f16c3005125536937
Georgii Medko <georgii.medko@gmail.com>
2023-04-30 16:07:32 +0500

    code according to PEP8

---
+++ test.py
@@ -0,0 +1,8 @@
+# def we_crash_all(name: str) -> str:
+#     return 'Привет, ' + name + ', мы всё сломали!'
+#
+# print(we_crash_all(100))
+
+
+def remove_rep(current_rep: float, rep_points: int,
+               debuf_effect: bool) -> float:
